e8c63ad7d2d84f679a96a73ff98d1db8087e1dfe
JDom12 <dominguezjulian12@gmail.com>
2025-03-11 16:56:49 -0400

    partial character component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,12 @@ import { useEffect, useState } from 'react'
 import { BrowserRouter as Router, Routes, Route,  } from 'react-router-dom'
 import './App.css'
 import Home from './Home'
+import Character from './compnents/Character';
+
 
 function App() {
   const [characters, setCharacters] = useState(null);
-  
+  const [films, setFilms] = useState(null)
 
   const fetchCharacters = async () => {
     const response = await fetch('http://localhost:3000/api/characters');
@@ -13,14 +15,22 @@ function App() {
     setCharacters(data)
   }
 
+  const fetchFilms = async () => {
+    const response = await fetch('http://localhost:3000/api/films');
+    const data = await response.json();
+    setFilms(data)
+  }  
+
   useEffect(() => {
     fetchCharacters()
+    fetchFilms()
   }, [])
 
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<Home characters={characters}/>} />
+        <Route exact path="/character/:id" element={<Character films={films}/>} />
       </Routes>
     </Router>
   )
---
+++ client/src/compnents/Character.jsx
@@ -0,0 +1,72 @@
+import { useParams } from "react-router-dom"
+import { useEffect, useState } from 'react'
+
+
+const Character = ({films}) => {
+
+    const params = useParams()
+    const id = params.id
+
+    const [character, setCharacter] = useState(null)
+    const [planet, setPlanet] = useState(null)
+    const [characterFilms, setCharacterFilms] = useState([])
+
+    useEffect(() => { 
+        fetchCharacter(id) 
+        //fetchFilm(id)
+    }, [])
+
+    useEffect(() => { 
+        if(character?.homeworld){
+            fetchPlanet(character.homeworld) 
+        }
+    }, [character])
+
+
+
+
+    async function fetchCharacter(id) {
+        let response = await fetch(`http://localhost:3000/api/characters/${id}`)
+        const data = await response.json()
+        setCharacter(data)
+    }
+
+    async function fetchPlanet(id) {
+        let response = await fetch(`http://localhost:3000/api/planets/${id}`)
+        const data = await response.json()
+        setPlanet(data)
+    }
+
+    async function fetchFilm(id) {
+        let response = await fetch(`http://localhost:3000/api/characters/${id}/films`)
+        const data = await response.json()
+        const filteredFilms = films?.filter(item => item.id !== data.film_id)
+
+        setCharacterFilms(filteredFilms)
+    }
+
+    return (
+        <>
+            <h1 id="name"></h1>
+            <section id="generalInfo">
+                <p>Height: <span id="height">{character?.height}</span> cm</p>
+                <p>Mass: <span id="mass">{character?.mass}</span> kg</p>
+                <p>Born: <span id="birth_year">{character?.birth_year}</span></p>
+            </section>
+            <section id="planets">
+                <h2>Homeworld</h2>
+                <p><span id="homeworld">{planet?.name}</span></p>
+            </section>
+            <section id="films">
+                <h2>Films appeared in</h2>
+                <div>
+                    {characterFilms?.map(
+                        item => <div>{item.name}</div>
+                    )}
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Character
